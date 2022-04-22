af74361dd42a519e65ea4829c85e16e1aed78c81
Suhaib Ansari <suhaibansari@ku.edu>
2022-04-21 20:47:42 -0500

    nothing

--- spotify/auth.py
+++ spotify/auth.py
@@ -2,6 +2,8 @@ import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy.util as util
+import json
+
 client_id = '6e0bcc879be24b1c8bff71095368e345'
 client_secret = '98602d839f69491681956e0989fbdbb9'
 redirect_uri = "http://localhost:4200/"
@@ -21,13 +23,14 @@ user_name = str(sp.current_user()['display_name'])
 while playlists:
     for i, playlist in enumerate(playlists['items']):
         # sp.playlist_add_items(playlist_id=playlist['id'],items=[link], position=None)
-        item = sp.playlist_tracks(playlist['id'], fields=None, limit=50, offset=0)
+        item = sp.playlist_tracks(playlist['id'], fields=None, limit=None, offset=0)
         entry = list()
-        for n in range(5):
+        for n in range(100):
             try:
                 song = str(item['items'][n]['track']['name'])
                 artist = str(item['items'][n]['track']['artists'][0]['name'])
-                insertion = f"{song} by {artist}"
+                # insertion = f"{song} by {artist}"
+                insertion = song
                 entry.append(insertion)
             except:
                 pass
@@ -41,5 +44,7 @@ while playlists:
     else:
         playlists = None
 
-print(my_dict) # this dictionary contains the playlist copies with the name of the playlist as the key
+for each in my_dict[f'GOAT TIER ANIME OPENINGS, by {user_name}:{user_id}']:
+    print(each) # this dictionary contains the playlist copies with the name of the playlist as the key
 # so far, we can retrieve all of the relevant info for swapstream through the above code
+print(json.dumps(my_dict))
