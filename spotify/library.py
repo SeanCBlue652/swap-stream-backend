a9b3f9b7c342342c23bee942f4b1ccca7c2044f0
Suhaib Ansari <suhaibansari@ku.edu>
2022-04-24 23:30:39 -0500

    matched library

--- spotify/library.py
+++ spotify/library.py
@@ -32,18 +32,17 @@ class Library:
                 for n in range(100):
                     try:
                         song = str(item['items'][n]['track']['name'])
-                        print(song)
                         artist = str(item['items'][n]['track']['artists'][0]['name'])
-                        # insertion = f"{song} by {artist}"
-                        insertion = song
-                        entry.append(song)
+                        insertion = list()
+                        insertion.append(song)
+                        insertion.append(artist)
+                        entry.append(insertion)
                     except:
                         pass
                 
                 list_entries = list()
                 list_entries.append(stack)
                 the_plist.append(entry)
-                key = f"{plist_name}"
                 my_dict["info"] = list_entries
                 my_dict["playlists"] = the_plist
                 print(my_dict)
@@ -58,7 +57,10 @@ class Library:
     def search(self, key):
         return self.playlist_dict[key]
     
-    ''' song is gonna be a 3 entry array containing song name, uri, and artist'''
+    ''' 
+        Queries spotify for song id to add to list
+        :param song: list containing song name, song link on spotify, and song artist
+    '''
     def searchSpotify(self, song):
         link = song[1]
         name = song[0]
@@ -99,6 +101,13 @@ class Library:
         output.append(name)
         output.append(self.playlist_dict)
         return json.dumps(self.playlist_dict)
+
+    '''
+        Can add playlist
+
+        :param info: the choices the user makes when adding the playlist
+        :param plist: the playlist to be added as a list of lists [name, id, artist]
+    '''
     
     def addPlist(self, info: dict, plist: list):
         this_list = self.sp.user_playlist_create(self.user_id, info["name"], info["public"], info["collaborative"], info["description"])
@@ -109,5 +118,5 @@ class Library:
                 items.append(link)
             except:
                 print(str(each[0]) + " could not be added to target playlist.")
-            self.sp.playlist_add_items(playlist_id=this_list['id'], items=items, position=None)
+        self.sp.playlist_add_items(playlist_id=this_list['id'], items=items, position=None)
         
