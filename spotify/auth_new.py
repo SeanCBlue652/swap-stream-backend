abe5fa12e87d944ba55d2f77f45d38361d3b287c
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2022-04-29 14:32:37 -0500

    attempted to test something

--- spotify/auth_new.py
+++ spotify/auth_new.py
@@ -3,8 +3,8 @@ from spotipy.oauth2 import SpotifyOAuth
 
 
 def run():
-    client_id = '6e0bcc879be24b1c8bff71095368e345'
-    client_secret = '98602d839f69491681956e0989fbdbb9'
+    client_id = '73484c021b8545c29225c24a1d8ee5e0'
+    client_secret = '91555a75e07c47ee816e39db529b0c10'
     redirect_uri = "http://localhost:4200/"
 
     scope = 'playlist-modify-public'
